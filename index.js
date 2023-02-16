52965bfcf4d671c16ce67b7d8303737c32da786e
Cristian Vazquez <vazquezcristianr@gmail.com>
2023-02-16 00:06:50 -0300

    actualizo

--- index.js
+++ index.js
@@ -1,94 +1,96 @@
-/*Juego de ahorcado, lo primero que tengo que hace es determinar una lista de palabras, que es desde donde se va a tomar una de manera aleatoria para comenzar el juego*/
+function ahorcado(){
+    /*Juego de ahorcado, lo primero que tengo que hace es determinar una lista de palabras, que es desde donde se va a tomar una de manera aleatoria para comenzar el juego*/
 
-/*Lista de palabras*/
+    /*Lista de palabras*/
 
-let listaPalabras=["microondas","heladera","televisor","completo","psicologo"]
+    let listaPalabras=["microondas","heladera","televisor","completo","psicologo"]
 
-/*Elijo la palabra, genero un indice aleatorio para el areglo*/
+    /*Elijo la palabra, genero un indice aleatorio para el areglo*/
 
-indice=Math.floor(Math.random()*listaPalabras.length)
-let palabraElegida=listaPalabras[indice]
+    indice=Math.floor(Math.random()*listaPalabras.length)
+    let palabraElegida=listaPalabras[indice]
 
-/*cuento la cantidad de letras q tiene la palabra para informarlo en el juego*/
+    /*cuento la cantidad de letras q tiene la palabra para informarlo en el juego*/
 
-numeroLetras=palabraElegida.length
+    numeroLetras=palabraElegida.length
 
-/*Defino la cantidad de vidas o intentos que va a tener*/
+    /*Defino la cantidad de vidas o intentos que va a tener*/
 
-vidas=6
+    vidas=6
 
 
-/* Funcion que agrega a una lista la letra elegida por el usuario. llevo todo a minusculas para que no haya error y corroboro que el ingreso del usuario sea una letra*/
-function elegirLetra(){
-    let letraElegida=prompt("Elija una letra").toLowerCase();
-    const regexp=/[a-z]/;
-    validacionLetra=regexp.test(letraElegida);
-    validacionCaracteres=letraElegida.length;
-    while (validacionLetra==false || validacionCaracteres!=1){
-        letraElegida=prompt("Usted no a elegido una letra o eligió mas de una. Ingrese una letra").toLowerCase();
+    /* Funcion que agrega a una lista la letra elegida por el usuario. llevo todo a minusculas para que no haya error y corroboro que el ingreso del usuario sea una letra*/
+    function elegirLetra(){
+        let letraElegida=prompt("Elija una letra").toLowerCase();
+        const regexp=/[a-z]/;
         validacionLetra=regexp.test(letraElegida);
         validacionCaracteres=letraElegida.length;
-    }
-    return letraElegida
-}
-
-/*creo una funcion para que cree una lista de las letras elgidas*/
-let listaUsuario=[]
-function listaLetras(letraUsuario){
-    listaUsuario.push(letraUsuario)
-    return listaUsuario
-}
-
-/* creo una funcion que me vaya agregando las letras elegidas a la palabra */
-let palabraArmada=[];
-palabraArmada.length=palabraElegida.length
-function completaPalabra(letra,palabraRandom){
-    let coincidencia=0
-    for (let j=0;j<palabraRandom.length;j++){
-        if(letra==palabraRandom.split('')[j]){
-            palabraArmada[j]=letra
-            coincidencia++
+        while (validacionLetra==false || validacionCaracteres!=1){
+            letraElegida=prompt("Usted no a elegido una letra o eligió mas de una. Ingrese una letra").toLowerCase();
+            validacionLetra=regexp.test(letraElegida);
+            validacionCaracteres=letraElegida.length;
         }
-        else{
-            continue
-        }
+        return letraElegida
     }
-    /*completo los valores vacios con guiones*/
-    for (let i=0;i<palabraArmada.length;i++){
-        if (palabraArmada[i]==null){
-            palabraArmada[i]="_"
-        }
-        else{
-            continue
-        }
+
+    /*creo una funcion para que cree una lista de las letras elgidas*/
+    let listaUsuario=[]
+    function listaLetras(letraUsuario){
+        listaUsuario.push(letraUsuario)
+        return listaUsuario
     }
-    let palabraCadena=palabraArmada.join(' ')
-    return [palabraCadena,coincidencia]
-}
 
-
-/*defino una funcion que me vaya completando las letras que va adivinando*/
-
-function PalabraGuiones(){
-    let palabraIncompleta="";
-    for(let i=0;i<numeroLetras;i++){
-        palabraIncompleta = "_" + " " + palabraIncompleta;
+    /* creo una funcion que me vaya agregando las letras elegidas a la palabra */
+    let palabraArmada=[];
+    palabraArmada.length=palabraElegida.length
+    function completaPalabra(letra,palabraRandom){
+        let coincidencia=0
+        for (let j=0;j<palabraRandom.length;j++){
+            if(letra==palabraRandom.split('')[j]){
+                palabraArmada[j]=letra
+                coincidencia++
+            }
+            else{
+                continue
+            }
+        }
+        /*completo los valores vacios con guiones*/
+        for (let i=0;i<palabraArmada.length;i++){
+            if (palabraArmada[i]==null){
+                palabraArmada[i]="_"
+            }
+            else{
+                continue
+            }
+        }
+        let palabraCadena=palabraArmada.join(' ')
+        return [palabraCadena,coincidencia]
     }
-    return palabraIncompleta
-}
-
-/*Funcion para actualizar algunos elementos del html */
-function actualizar(){
-    $("#image").load(" #image > *");
-    $("#letraElegidas").load(" #LetraElegidas > *");
-    $("#palabra").load(" #palabra > *");
-}
 
 
-/* comienzo a diseñar el juego, arranco por un mensaje introuctorio*/
+    /*defino una funcion que me vaya completando las letras que va adivinando*/
+
+    function PalabraGuiones(){
+        let palabraIncompleta="";
+        for(let i=0;i<numeroLetras;i++){
+            palabraIncompleta = "_" + " " + palabraIncompleta;
+        }
+        return palabraIncompleta
+    }
+
+    /*Funcion para actualizar algunos elementos del html */
+    function actualizar(){
+        $("#image").load(" #image > *");
+        $("#letraElegidas").load(" #LetraElegidas > *");
+        $("#palabra").load(" #palabra > *");
+        continue
+    }
 
 
-function ahorcado(){
+    /* comienzo a diseñar el juego, arranco por un mensaje introuctorio*/
+
+
+
     /*inicio el jugo y vuelvo al inicio las imagenes y los textos del html*/
     document.getElementById("image").src = "./imagenes/ahorcado 0.png";
     let leElegidas = document.getElementById('letrasElegidas');
@@ -155,7 +157,6 @@ function ahorcado(){
 
         }
         actualizar()
-
     }
 
 }
