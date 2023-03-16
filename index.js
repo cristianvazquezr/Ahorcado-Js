fcccec8ab9e48ed51b2359a83276cf018173822e
cristianvazquezr <vazquezcristianr@gmail.com>
2023-03-15 21:15:37 -0300

    agrego objetos, y aplico metodos de arrays

--- index.js
+++ index.js
@@ -3,28 +3,84 @@ function ahorcado(){
 
     /*Lista de palabras*/
 
-    let listaPalabras=["microondas","heladera","televisor","completo","psicologo"]
+/*creo una clase palabras para ir creando estas y definirle una categoria y una dificultad*/
+    class palabras{
+        constructor(categoria,dificultad,palabras){
+        this.categoria=categoria
+        this.dificultad=dificultad
+        this.palabras=palabras
+        }
+    }
+
+    /*voy agregando los objetos a una lista de objetos palabras */
+    const listaPalabras=[]
+
+    listaPalabras.push(new palabras("frutas","facil","Lechuga"))
+    listaPalabras.push(new palabras("frutas","facil","papa"))
+    listaPalabras.push(new palabras("frutas","dificil","mangostino"))
+    listaPalabras.push(new palabras("electrodomesticos","dificil","microondas"))
+    listaPalabras.push(new palabras("electrodomesticos","dificil","heladera"))
+    listaPalabras.push(new palabras("electrodomesticos","facil","radio"))
+    listaPalabras.push(new palabras("ciencia","dificil","mercurio"))
+    listaPalabras.push(new palabras("ciencia","dificil","otorrinolaringologia"))
+    listaPalabras.push(new palabras("ciencia","dificil","alcalinidad"))
+
+
+    /*creo una funcion que me permita elegir la dificultad de las palabras. Aca incluyo el saludo al iniciar el juego */
+
+    function palabraDificultad(){
+        let dificultad=parseInt(prompt("Hola, esta por comenzar a jugar al ahorcado.Ingrese la dificultad:\n-(1) facil\n-(2) dificil"))
+
+        while (!(dificultad==1 || dificultad==2)){
+            dificultad=parseInt(prompt("ingrese una opción válida"));
+        }
+        let dificultadPalabra="";
+        if (dificultad==1){
+            dificultadPalabra="facil"
+        }
+        else{
+            dificultadPalabra="dificil"
+        }
+        let listaPalabrasDificultad=[]
+        listaPalabras.forEach((palabritas) => {
+            if (palabritas.dificultad==dificultadPalabra){
+            listaPalabrasDificultad.push(palabritas.palabras)}      
+        });
+        return listaPalabrasDificultad
+    }
 
     /*Elijo la palabra, genero un indice aleatorio para el areglo*/
 
-    indice=Math.floor(Math.random()*listaPalabras.length)
-    let palabraElegida=listaPalabras[indice]
+    let indice=Math.floor(Math.random()*listaPalabras.length)
+    let palabraElegida=palabraDificultad()[indice]
+    
+
+    /* Busco la categoria de la palabra random elegida */
+    let categoria=""
+    let dificultadElegida=""
+    listaPalabras.forEach((palabra)=>{
+        if(palabra.palabras==palabraElegida){
+            categoria=palabra.categoria
+            dificultadElegida=palabra.dificultad
+        }
+    })
+
 
     /*cuento la cantidad de letras q tiene la palabra para informarlo en el juego*/
 
-    numeroLetras=palabraElegida.length
+    const numeroLetras=palabraElegida.length
 
     /*Defino la cantidad de vidas o intentos que va a tener*/
 
-    vidas=6
+    let vidas=6
 
 
     /* Funcion que agrega a una lista la letra elegida por el usuario. llevo todo a minusculas para que no haya error y corroboro que el ingreso del usuario sea una letra*/
     function elegirLetra(){
         let letraElegida=prompt("Elija una letra").toLowerCase();
         const regexp=/[a-z]/;
-        validacionLetra=regexp.test(letraElegida);
-        validacionCaracteres=letraElegida.length;
+        let validacionLetra=regexp.test(letraElegida);
+        let validacionCaracteres=letraElegida.length;
         while (validacionLetra==false || validacionCaracteres!=1){
             letraElegida=prompt("Usted no a elegido una letra o eligió mas de una. Ingrese una letra").toLowerCase();
             validacionLetra=regexp.test(letraElegida);
@@ -41,8 +97,7 @@ function ahorcado(){
     }
 
     /* creo una funcion que me vaya agregando las letras elegidas a la palabra */
-    let palabraArmada=[];
-    palabraArmada.length=palabraElegida.length
+    const palabraArmada=Array(numeroLetras);
     function completaPalabra(letra,palabraRandom){
         let coincidencia=0
         for (let j=0;j<palabraRandom.length;j++){
@@ -50,19 +105,11 @@ function ahorcado(){
                 palabraArmada[j]=letra
                 coincidencia++
             }
-            else{
-                continue
+            else if(palabraArmada[j]==null) {
+                palabraArmada[j]="_"
             }
         }
-        /*completo los valores vacios con guiones*/
-        for (let i=0;i<palabraArmada.length;i++){
-            if (palabraArmada[i]==null){
-                palabraArmada[i]="_"
-            }
-            else{
-                continue
-            }
-        }
+
         let palabraCadena=palabraArmada.join(' ')
         return [palabraCadena,coincidencia]
     }
@@ -79,9 +126,9 @@ function ahorcado(){
     }
 
 
-    /* comienzo a diseñar el juego, arranco por un mensaje introuctorio*/
+    /* Luego de dar la bienvenida al juego, arranco por un mensaje introuctorio*/
 
-
+    alert(`Usted tiene 6 vidas y la dificultad elegida fue ${dificultadElegida}. Mucha Suerte!`)
 
     /*inicio el jugo y vuelvo al inicio las imagenes y los textos del html*/
     document.getElementById("image").src = "./imagenes/ahorcado 0.png";
@@ -90,14 +137,12 @@ function ahorcado(){
     document.getElementById('tituloLetras').innerHTML="";
     leElegidas.innerHTML = "";
     palabraUsuario.innerHTML = "";
-
-    alert("Hola, esta por comenzar a jugar al ahorcado, tenes 6 vidas. Suerte!!.")
     let gana=false
     let listLetraElegida=[];
     let letraElegida=""
     let palabraArmando=completaPalabra(letraElegida,palabraElegida)[0];
     let coincidencias= completaPalabra(letraElegida,palabraElegida)[1];
-    console.log("La palabra elegida tiene " + numeroLetras + " letras.\n" + palabraArmando);
+    console.log("La palabra elegida tiene " + numeroLetras + " letras.\nPertenece a la categoria " + categoria + "\n" + palabraArmando);
       
     while (vidas>0 && gana==false){
         letraElegida=elegirLetra();
